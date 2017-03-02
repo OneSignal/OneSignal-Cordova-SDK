aaeb0f79149036d810a56b7ed912bde971b2f662
Josh Kasten <jkasten@gmail.com>
2017-03-02 14:04:55 -0800

    Removed Google Project number param from example

--- Example/www/js/index.js
+++ Example/www/js/index.js
@@ -54,7 +54,7 @@ var app = {
         iosSettings["kOSSettingsKeyAutoPrompt"] = true;
         iosSettings["kOSSettingsKeyInAppLaunchURL"] = false;
 
-        window.plugins.OneSignal.startInit( "5eb5a37e-b458-11e3-ac11-000c2940e62c", "703322744261")
+        window.plugins.OneSignal.startInit("5eb5a37e-b458-11e3-ac11-000c2940e62c")
                                 .handleNotificationReceived(didReceiveRemoteNotificationCallBack)
                                 .handleNotificationOpened(didOpenRemoteNotificationCallBack)
                                 .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
