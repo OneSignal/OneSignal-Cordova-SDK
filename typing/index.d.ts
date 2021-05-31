babcc7137702204926cf6e628c2e5bec7e472b79
Jeasmine Nahui <jeas.nahui@gmail.com>
2021-05-31 20:41:42 -0300

    Add TypeScript support
    * Create typing files
    * Rename OneSignal.js to OneSignalPlugin.js
    * Export typed OneSignal

---
+++ typing/index.d.ts
@@ -0,0 +1,8 @@
+import { OSNotification, NotificationReceivedEvent, OpenedEvent, OpenedEventAction, OpenedEventActionType } from './Notification';
+import { OutcomeEvent } from './Outcomes';
+import { PermissionChange, SubscriptionChange, EmailSubscriptionChange, SMSSubscriptionChange, DeviceState, IosPermissionStatus } from './Subscription';
+import { LogLevel, ChangeEvent } from './Extras';
+import { OneSignalPlugin } from './OneSignalPlugin';
+export declare const OneSignal: OneSignalPlugin;
+export { OneSignalPlugin };
+export default OneSignal;
