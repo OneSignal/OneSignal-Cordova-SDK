ac9bfacef8b3e7a98a8bcb3e79e204f220355cad
Jeasmine Nahui <jeas.nahui@gmail.com>
2021-06-11 15:30:08 -0300

    Code review comments
    * Add new lines to end of file
    * Add InAppMessage ts
    * Rename typing directory to types

---
+++ types/InAppMessage.d.ts
@@ -0,0 +1,8 @@
+export interface InAppMessageAction {
+    closes_message  : boolean;
+    first_click     : boolean;
+    click_name      ?: string;
+    click_url       ?: string;
+    outcomes        ?: object[];
+    tags            ?: object;
+}
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,6 @@
 import { OSNotification, NotificationReceivedEvent, OpenedEvent, OpenedEventAction, OpenedEventActionType } from './Notification';
 import { OutcomeEvent } from './Outcomes';
+import { InAppMessageAction } from './InAppMessage';
 import { PermissionChange, SubscriptionChange, EmailSubscriptionChange, SMSSubscriptionChange, DeviceState, IosPermissionStatus } from './Subscription';
 import { LogLevel, ChangeEvent } from './Extras';
 import { OneSignalPlugin } from './OneSignalPlugin';
