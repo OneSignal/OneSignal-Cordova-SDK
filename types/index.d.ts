1805c895f33a19e926d798d8d34ef74a6942e465
Jeasmine Nahui <jeas.nahui@gmail.com>
2021-06-14 15:18:06 -0300

    Remove unnecessary imports

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,3 @@
-import { OSNotification, NotificationReceivedEvent, OpenedEvent, OpenedEventAction, OpenedEventActionType } from './Notification';
-import { OutcomeEvent } from './Outcomes';
-import { InAppMessageAction } from './InAppMessage';
-import { PermissionChange, SubscriptionChange, EmailSubscriptionChange, SMSSubscriptionChange, DeviceState, IosPermissionStatus } from './Subscription';
-import { LogLevel, ChangeEvent } from './Extras';
 import { OneSignalPlugin } from './OneSignalPlugin';
 export declare const OneSignal: OneSignalPlugin;
 export { OneSignalPlugin };
