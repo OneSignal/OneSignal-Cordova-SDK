11ebb1429135151de457d6095e91e46eb7a5bba9
Nan <nanftw@gmail.com>
2022-06-27 04:45:32 -0700

    Make file for `OSNotification`
    * Create OSNotification.ts file and class
    
    * Add missing Android OSNotification.groupedNotifications property
    
    * Remove backgroundImageLayout, OSAndroidBackgroundImageLayout as it is does not exist (anymore?)
    
    * Update NotificationReceived.js to remove OSNOtification

--- types/Notification.d.ts
+++ types/Notification.d.ts
@@ -1,44 +1,6 @@
 // 0 = NotificationClicked, 1 = ButtonClicked
 export type OpenedEventActionType = 0 | 1;
 
- /* N O T I F I C A T I O N S */
-export interface OSNotification {
-    body            : string;
-    sound           ?: string;
-    title           ?: string;
-    launchURL       ?: string;
-    rawPayload      : object;
-    actionButtons   ?: object[];
-    additionalData  : object;
-    notificationId  : string;
-    // android only
-    groupKey                ?: string;
-    groupMessage            ?: string;
-    ledColor                ?: string;
-    priority                ?: number;
-    smallIcon               ?: string;
-    largeIcon               ?: string;
-    bigPicture              ?: string;
-    collapseId              ?: string;
-    fromProjectNumber       ?: string;
-    smallIconAccentColor    ?: string;
-    lockScreenVisibility    ?: string;
-    androidNotificationId   ?: number;
-    // ios only
-    badge               ?: string;
-    badgeIncrement      ?: string;
-    category            ?: string;
-    threadId            ?: string;
-    subtitle            ?: string;
-    templateId          ?: string;
-    templateName        ?: string;
-    attachments         ?: object;
-    mutableContent      ?: boolean;
-    contentAvailable    ?: string;
-    relevanceScore      ?: number;
-    interruptionLevel   ?: string;
-}
-
 /* N O T I F I C A T I O N   &   I A M   E V E N T S */
 export interface NotificationReceivedEvent {
     complete        : (notification?: OSNotification | null) => void;
