622ccb0fe0d94ffbde7762815edf3eeb227c8287
Nan <nanftw@gmail.com>
2022-06-27 04:59:10 -0700

    Make file for `NotificationReceivedEvent`
    * Create NotificationReceivedEvent.ts file and class
    * Update NotificationReceived.js to remove NotificationReceivedEvent
    * Have Android bridge return OSNotificationReceivedEvent to callback instead of OSNotificationReceivedEvent, so we don't have to do another check for OSNotificationReceivedEvent.notification to grab it.
    * Update NotificationReceived.js to remove OSNotificationReceivedEvent

--- www/NotificationReceived.js
+++ www/NotificationReceived.js
@@ -14,38 +14,3 @@ class OSActionButton {
         }
     }
 }
-
-var OSNotificationReceivedEvent = {
-    notification: null,
-    create : function (receivedEvent) {
-        if (receivedEvent.notification) {
-            // Android case
-            this.notification = new OSNotification(receivedEvent.notification);
-        } else {
-            // iOS case
-            this.notification = new OSNotification(receivedEvent);
-        }
-        return this;
-    },
-    complete : function (notification) {
-        if (!notification) {
-            // if the notificationReceivedEvent is null, we want to call the native-side
-            // complete/completion with null to silence the notification
-            cordova.exec(function(){}, function(){}, "OneSignalPush", "completeNotification", [this.notification.notificationId, false]);
-            return;
-        }
-
-        // if the notificationReceivedEvent is not null, we want to pass the specific event
-        // future: Android side: make the notification modifiable
-        // iOS & Android: the notification id is associated with the native-side complete handler / completion block
-        cordova.exec(function(){}, function(){}, "OneSignalPush", "completeNotification", [this.notification.notificationId, true]);
-    },
-    getNotification: function() {
-        return this.notification; 
-    }
-};
-
-module.exports = {
-    OSNotification: OSNotification,
-    OSNotificationReceivedEvent: OSNotificationReceivedEvent
-};
