935d36a7687630b507b17a454fe4bedcf508f86f
Nan <nanftw@gmail.com>
2022-06-28 17:42:52 -0700

    `index.ts` - turn off type checking for `window` (step 4)
    * Turn off typechecking and errors about window.cordova and window.plugins
    
    - Property 'cordova' does not exist on type 'Window & typeof globalThis'.
    
    - Property 'plugins' does not exist on type 'Window & typeof globalThis'.

--- www/index.ts
+++ www/index.ts
@@ -36,6 +36,14 @@ import {
     SubscriptionChange
 } from "./Subscription";
 
+// Suppress TS warnings about window.cordova
+declare global {
+    interface Window {
+        cordova: any; // turn off type checking
+        plugins: any;
+    }
+}
+
 // 0 = None, 1 = Fatal, 2 = Errors, 3 = Warnings, 4 = Info, 5 = Debug, 6 = Verbose
 export type LogLevel = 0 | 1 | 2 | 3 | 4 | 5 | 6;
 
