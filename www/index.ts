a7bc87b6081f3424c62a026dd5722a2c27a69f1e
Wilgert Velinga <wilgert@wilgert.nl>
2022-08-09 13:11:56 +0200

    export OneSignalPlugin so it can be imported in consumer typescript code

--- www/index.ts
+++ www/index.ts
@@ -50,7 +50,7 @@ declare global {
 // 0 = None, 1 = Fatal, 2 = Errors, 3 = Warnings, 4 = Info, 5 = Debug, 6 = Verbose
 export type LogLevel = 0 | 1 | 2 | 3 | 4 | 5 | 6;
 
-class OneSignalPlugin {
+export class OneSignalPlugin {
     private _appID = "";
     private _notificationWillShowInForegroundDelegate = function(notificationReceived: NotificationReceivedEvent) {};
     private _notificationOpenedDelegate = function(notificationOpened: OpenedEvent) {};
