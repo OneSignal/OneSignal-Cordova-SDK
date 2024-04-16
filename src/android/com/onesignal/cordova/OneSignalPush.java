99659c4662aef17878b3474cfca30f33607daa3a
Nan <nanftw@gmail.com>
2024-04-16 13:22:40 -0700

    release commit 5.1.2

--- src/android/com/onesignal/cordova/OneSignalPush.java
+++ src/android/com/onesignal/cordova/OneSignalPush.java
@@ -348,7 +348,7 @@ public class OneSignalPush extends CordovaPlugin implements INotificationLifecyc
 
   public boolean init(CallbackContext callbackContext, JSONArray data) {
     OneSignalWrapper.setSdkType("cordova");  
-    OneSignalWrapper.setSdkVersion("050101");
+    OneSignalWrapper.setSdkVersion("050102");
     try {
       String appId = data.getString(0);
       OneSignal.initWithContext(this.cordova.getActivity(), appId);
