ed6b6dd1516d284c7951cb01285252a55cc4e96a
Jenna Antilla <46546946+jennantilla@users.noreply.github.com>
2024-05-08 20:20:55 -0700

    Add Notification Service Extension

---
+++ example/IonicCapOneSignal/ios/App/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Jenna Antilla on 5/8/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
