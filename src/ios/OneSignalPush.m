b1d07106ed4192681a95adc87ad02d1b68058737
Jenna Antilla <46546946+jennantilla@users.noreply.github.com>
2024-05-31 12:19:13 -0700

    release commit

--- src/ios/OneSignalPush.m
+++ src/ios/OneSignalPush.m
@@ -107,7 +107,7 @@ void processNotificationClicked(OSNotificationClickEvent* event) {
 
 void initOneSignalObject(NSDictionary* launchOptions) {
     OneSignalWrapper.sdkType = @"cordova";
-    OneSignalWrapper.sdkVersion = @"050104";
+    OneSignalWrapper.sdkVersion = @"050200";
     [OneSignal initialize:nil withLaunchOptions:launchOptions];
     initialLaunchFired = true;
 }
