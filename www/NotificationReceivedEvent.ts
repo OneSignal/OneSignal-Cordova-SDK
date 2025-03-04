4bcc0777baabe5df5ef1e10422aef21a962b9aeb
Fadi George <fadii925@gmail.com>
2025-03-04 13:36:40 -0800

    address pr comments

--- www/NotificationReceivedEvent.ts
+++ www/NotificationReceivedEvent.ts
@@ -12,13 +12,21 @@ export class NotificationWillDisplayEvent {
     this.notification = new OSNotification(displayEvent);
   }
 
-  preventDefault(shouldDiscard: boolean = false): void {
+  /**
+   * Call this to prevent OneSignal from displaying the notification automatically.
+   * This method can be called up to two times with false and then true, if processing time is needed.
+   * Typically this is only possible within a short
+   * time-frame (~30 seconds) after the notification is received on the device.
+   * @param discard an [preventDefault] set to true to dismiss the notification with no
+   * possibility of displaying it in the future.
+   */
+  preventDefault(discard: boolean = false): void {
     window.cordova.exec(
       function () {},
       function () {},
       "OneSignalPush",
       "preventDefault",
-      [this.notification.notificationId, shouldDiscard]
+      [this.notification.notificationId, discard]
     );
     return;
   }
