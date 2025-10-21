d234d8f0dc77cc4165a0a3df469510d1e4da73d1
Fadi George <fadii925@gmail.com>
2025-10-21 16:01:05 -0700

    Revert "remove test related items"
    This reverts commit e411dba93418f44356f3c92a920a9249ebb5a54c.

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    include: ['**/*.test.ts', '**/*.test.tsx'],
+    coverage: {
+      enabled: true,
+      reporter: ['text-summary', 'lcov'],
+      reportOnFailure: true,
+      reportsDirectory: 'coverage',
+    },
+  },
+});
