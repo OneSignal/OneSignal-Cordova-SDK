f3f27446e231fe8b28ae9b7934a26a249d7bd715
Fadi George <fadii925@gmail.com>
2025-11-21 14:56:49 -0800

    add onesignal plugin to angular example

---
+++ example/IonicAngular/scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd ../../
+bun run build
+
+# Pack
+bun pm pack
+mv onesignal-cordova-plugin-*.tgz onesignal-cordova-plugin.tgz
+
+# Install/update the plugin in the example app
+cd example/IonicAngular
+if [ -n "$CI" ]; then
+  bun install --frozen-lockfile
+else
+  bun i
+fi
+
+# Build example
+ng build
+
