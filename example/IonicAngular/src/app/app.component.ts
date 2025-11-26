4289914d733ca291239353013b4e63bd345569d9
Fadi George <fadii925@gmail.com>
2025-11-26 13:42:59 -0800

    add ios folder

--- example/IonicAngular/src/app/app.component.ts
+++ example/IonicAngular/src/app/app.component.ts
@@ -16,6 +16,6 @@ export class AppComponent {
     const APP_ID = '77e32082-ea27-42e3-a898-c72e141824ef'; // Use your app ID
     OneSignal.Debug.setLogLevel(LogLevel.Verbose);
     OneSignal.initialize(APP_ID);
-    OneSignal.LiveActivities.setupDefault();
+    OneSignal.login('ionic-angular-8');
   }
 }
