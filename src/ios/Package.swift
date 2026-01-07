300632f5cd7ca7356909a93ba27fb1eaccc2f884
Fadi George <fadii925@gmail.com>
2026-01-06 17:54:31 -0800

    tweak packageswift config

--- src/ios/Package.swift
+++ src/ios/Package.swift
@@ -2,36 +2,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "OneSignalCordovaPlugin",
+    name: "onesignal-cordova-plugin",
     platforms: [
-        .iOS(.v11)
+        .iOS("12.0")
     ],
     products: [
-        .library(
-            name: "OneSignalCordovaPlugin",
-            targets: ["OneSignalCordovaPlugin"]
-        )
+        .library(name: "onesignal-cordova-plugin", targets: ["onesignal_cordova_plugin"])
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/OneSignal/OneSignal-XCFramework.git",
-            exact: "5.2.15"
-        )
+        .package(url: "https://github.com/OneSignal/OneSignal-XCFramework", exact: "5.2.15")
     ],
     targets: [
         .target(
-            name: "OneSignalCordovaPlugin",
+            name: "onesignal_cordova_plugin",
             dependencies: [
                 .product(name: "OneSignalFramework", package: "OneSignal-XCFramework"),
+                .product(name: "OneSignalInAppMessages", package: "OneSignal-XCFramework"),
+                .product(name: "OneSignalLocation", package: "OneSignal-XCFramework"),
                 .product(name: "OneSignalLiveActivities", package: "OneSignal-XCFramework")
             ],
-            path: ".",
-            sources: ["OneSignalPush.m"],
-            publicHeadersPath: ".",
-            cSettings: [
-                .headerSearchPath(".")
-            ]
+            path: "."
         )
     ]
 )
-
