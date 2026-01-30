1c7b003e2995a9bd4423ed9a6200989d71ad125c
Fadi George <fadii925@gmail.com>
2026-01-30 10:33:06 -0800

    remove filtering out nulls for android

--- src/android/com/onesignal/cordova/OneSignalController.java
+++ src/android/com/onesignal/cordova/OneSignalController.java
@@ -370,10 +370,7 @@ public class OneSignalController {
         while (keys.hasNext()) {
             String key = keys.next();
             Object value = json.get(key);
-            // Skip null values as the native SDK doesn't accept them
-            if (value != JSONObject.NULL) {
-                map.put(key, convertJsonValue(value));
-            }
+            map.put(key, convertJsonValue(value));
         }
 
         return map;
@@ -381,17 +378,15 @@ public class OneSignalController {
 
     /** Convert JSON value to native Java type */
     private static Object convertJsonValue(Object value) throws JSONException {
-        if (value instanceof JSONObject) {
+        if (value == JSONObject.NULL) {
+            return null;
+        } else if (value instanceof JSONObject) {
             return jsonObjectToMap((JSONObject) value);
         } else if (value instanceof JSONArray) {
             JSONArray jsonArray = (JSONArray) value;
             Collection<Object> list = new ArrayList<>();
             for (int i = 0; i < jsonArray.length(); i++) {
-                Object item = jsonArray.get(i);
-                // Skip null values in arrays
-                if (item != JSONObject.NULL) {
-                    list.add(convertJsonValue(item));
-                }
+                list.add(convertJsonValue(jsonArray.get(i)));
             }
             return list;
         } else {
