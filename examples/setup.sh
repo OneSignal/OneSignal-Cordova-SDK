b54f0b1cd843ab644aba45c645be50dc3365b20b
Fadi George <fadii925@gmail.com>
2026-02-18 15:33:46 -0800

    update setup script

---
+++ examples/setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ORIGINAL_DIR=$(pwd)
+
+# Build root package
+cd ../../
+bun run build
+
+rm -f onesignal-cordova-plugin.tgz
+bun pm pack
+mv onesignal-cordova-plugin-*.tgz onesignal-cordova-plugin.tgz
+
+# Use fresh install of the package
+cd "$ORIGINAL_DIR"
+bun pm cache rm
+bun i
