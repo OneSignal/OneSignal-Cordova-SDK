8f674bf9ec5025d7e61c8e88c6c7bc21ca8b3b5f
Nan <nan@onesignal.com>
2026-09-22 16:21:43 -0700

    Bump the release version to 5.6.0
    The pipeline bumps the patch version by default. Raising the iOS deployment target from 12.0 to 15.0 drops apps that target iOS 12 to 14, so this release ships as a minor.

--- src/ios/OneSignalPush.m
+++ src/ios/OneSignalPush.m
@@ -119,7 +119,7 @@ void processNotificationClicked(OSNotificationClickEvent *event) {
 
 void initOneSignalObject(NSDictionary *launchOptions) {
   OneSignalWrapper.sdkType = @"cordova";
-  OneSignalWrapper.sdkVersion = @"050510";
+  OneSignalWrapper.sdkVersion = @"050600";
   [OneSignal initialize:nil withLaunchOptions:launchOptions];
 }
 
